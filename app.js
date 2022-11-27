ffb405f6a793b44f4329ab2fdad268a9daf11623
sreekaranne1 <sreekaranne@Sreekars-MacBook-Pro.local>
2022-11-27 12:08:11 -0500

    assignement 9

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ import HelloController from "./controllers/hello-controller.js";
 import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from "cors";
+import mongoose from "mongoose";
+const CONNECTION_STRING =
+  process.env.DB_CONNECTION_STRING || "mongodb://localhost:27017/tuiter";
+
+mongoose.connect(CONNECTION_STRING);
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 TuitsController(app);
 HelloController(app);
 UserController(app);
-app.listen(process.env.PORT|| 4000);
+app.listen(process.env.PORT || 4000);
